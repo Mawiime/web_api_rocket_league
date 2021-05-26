1e11fdc89980995e3486098aab91c82c6ed44f04
Fardel Hugo <hugo.fardel@live.fr>
2021-05-26 22:37:01 +0200

    css forms

--- front/src/components/AddItemComponent.vue
+++ front/src/components/AddItemComponent.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="addItem">
     <h1>Add new item</h1>
-        <input type="text" v-model="itemName"/>
-        <input type="text" v-model="itemType"/>
-        <input type="text" v-model="itemImage"/>
+        <ul v-if="error" class="errors">
+          <li v-for="item in logs" :key="item">{{ item }}</li>
+        </ul>
+        <input type="text" v-model="itemName" placeholder="Item name"/>
+        <input type="text" v-model="itemType" placeholder="Item type"/>
+        <input type="text" v-model="itemImage" placeholder="Item link image"/>
 
         <input type="button" @click="postItem" value="Post item"/>
   </div>
@@ -17,24 +20,34 @@ export default {
         return {
             itemName : null,
             itemType : null,
-            itemImage : null
-        }
+            itemImage : null,
+            logs: [],
+			      error: false
+        };
   },
   methods: {
       postItem(){
-          const data = {
-              itemName : this.itemName,
-              itemType : this.itemType,
-              itemImage : this.itemImage
+          this.logs = [];
+          if((this.itemName == null) || (this.itemType == null) || (this.itemImage == null)){
+            this.error = true;
+            this.logs.push("Au moins un champs n'est pas renseigné");
           }
+          else{
+            this.error = false;
+            const data = {
+                itemName : this.itemName,
+                itemType : this.itemType,
+                itemImage : this.itemImage
+            }
 
-          axios.post('http://localhost:3000/items', data)
-          .then((res) => {
-                console.log(`Status: ${res.status}`);
-                console.log('Body: ', res.data);
-          }).catch((err) => {
-                console.error(err);
-          })
+            axios.post('http://localhost:3000/items', data)
+            .then((res) => {
+                  console.log(`Status: ${res.status}`);
+                  console.log('Body: ', res.data);
+            }).catch((err) => {
+                  console.error(err);
+            })
+          }
       }
   }
 }
@@ -43,8 +56,54 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-h1{
+h1 {
   margin-bottom: 25px;
+  width: 500px;
+}
+
+input[type="text"] {
+  border: none;
+  width: 100%;
+  height: 100%;
+  padding: 10px 20px;
+  background: #333;
+  border-radius: 3px;
+  box-shadow: 0px 8px 15px rgba(#4b4848, 0.1);
+  margin: 20px 0;
+  color: white;
+}
+
+input[type="text"]:focus {
+  box-shadow: 0px 20px 30px rgba(#4b4848, 0.3);
+  outline: none;
+}
+
+input[type="button"] {
+  background-color: var(--main-color);
+  width: 100%;
+  border: none;
+  outline: none;
+  color: white;
+  padding: 10px 20px;
+  //text-transform: uppercase;
+  font-size: 16px;
+  margin: 40px 0;
+  cursor: pointer;
+  border-radius: 2px;
+  box-shadow: 0px 8px 15px rgba(#4b4848, 0.1);
+}
+
+input[type="button"]:hover {
+  opacity: 0.8;
+  box-shadow: 0px 20px 30px rgba(#4b4848, 0.3);
+}
+
+.errors{
+	background: #f8d7da;
+	color: #721c24;
+	padding: 8px 30px;
+	border-radius: 2px;
+	margin-bottom: 20px;
 }
 
 </style>
